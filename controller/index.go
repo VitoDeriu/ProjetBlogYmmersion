d131851547d2808e92da6b447dbcb9a9066397e6
dvito <vito.deriu@ynov.com>
2024-01-08 11:52:25 +0100

    test json to go

--- controller/index.go
+++ controller/index.go
@@ -2,8 +2,18 @@ package controller
 
 import (
 	InitTemp "ProjetBlogYmmersion/temps"
+	"encoding/json"
 	"net/http"
 )
+
+var article ArtStruct
+
+func GetDataFromJson() {
+
+	os.ReadFile("ProjetBlogYmmersion/data")
+
+	json.Unmarshal(article, &article)
+}
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	InitTemp.Temp.ExecuteTemplate(w, "home", nil)
---
+++ data/struct.go
@@ -0,0 +1,10 @@
+package data
+
+type ArtStruct struct {
+	Name    string `json:"name"`
+	Id      string `json:"id"`
+	Img     string `json:"img"`
+	Auteur  string `json:"auteur"`
+	Date    string `json:"date"`
+	Content string `json:"content"`
+}

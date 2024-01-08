4cac9b2ea043d3499cf285e5771ea345a60b7849
Yoan Commeau <yoan.commeau@ynov.com>
2024-01-08 16:02:00 +0100

    add randomArticle

--- controller/index.go
+++ controller/index.go
@@ -5,6 +5,7 @@ import (
 	InitTemp "ProjetBlogYmmersion/temps"
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"os"
 )
@@ -22,7 +23,30 @@ func GetDataFromJson() {
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
+
+	var dato []data.ArtStruct
 	GetDataFromJson()
-	fmt.Println(Articles)
-	InitTemp.Temp.ExecuteTemplate(w, "home", Articles)
+	dato = append(dato, Articles[1])
+	//fmt.Println(Articles)
+	//fmt.Println(dato)
+	RandomArticle()
+	RandomArticle()
+	RandomArticle()
+	RandomArticle()
+	fmt.Println("---------------")
+	InitTemp.Temp.ExecuteTemplate(w, "article", dato)
+}
+
+func RandomArticle() {
+	var randomart []data.ArtStruct
+	randomIndex := rand.Intn(len(Articles))
+
+	for _, i := range randomart {
+		if Articles[randomIndex] == i {
+			//RandomArticle()
+		} else {
+			randomart = append(randomart, Articles[randomIndex])
+		}
+	}
+	fmt.Println(randomart)
 }

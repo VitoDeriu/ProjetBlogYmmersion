93fead2d4d567caf09a70874f9c7667cfa8eb904
dvito <vito.deriu@ynov.com>
2024-01-11 09:37:11 +0100

    pour arthur

--- controller/categorie.go
+++ controller/categorie.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
-func Categorie (w http.ResponseWriter, r *http.Request){
+func Categorie(w http.ResponseWriter, r *http.Request) {
+	println(r.URL.Query().Get("type"))
 	temps.Temp.ExecuteTemplate(w, "categorie", nil)
 }
